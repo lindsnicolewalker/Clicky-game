3c6d5fd0f3a834e08884a99c1b46a4272e36ad91
lindsnicolewalker <linds.nicolewalker@gmail.com>
2019-01-30 19:50:00 -0800

    Updates

---
+++ precache-manifest.74d1208f4135c13ba1b2a465d56668f2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce688ce518f0ef6b5840",
+    "url": "/clicky-game/static/js/main.ce688ce5.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clicky-game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "16a45dbf8332c48be83100ab8848e285",
+    "url": "/clicky-game/index.html"
+  }
+];
